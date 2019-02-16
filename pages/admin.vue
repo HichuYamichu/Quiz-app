450bf5aa2c3c87e32903ad1bce8fd050d9c9a4af
Hichu Yamichu <HichuYamichu@gmail.com>
2019-02-16 14:18:53 +0100

    admin view done

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,9 +1,22 @@
 <template>
-	<div>
-		<new-collection-form/>
-		<edit-collection-form/>
-		<delete-collection-form/>
-	</div>
+	<v-layout>
+		<v-flex>
+			<v-toolbar>
+					<v-btn flat block large @click="showNewCollection">
+						New
+					</v-btn>
+					<v-btn flat block large @click="showEditCollection">
+						edit
+					</v-btn>
+					<v-btn flat block large @click="showDeleteCollection">
+						delete
+					</v-btn>
+			</v-toolbar>
+			<new-collection-form v-if="newCollection" />
+			<edit-collection-form v-if="editCollection" />
+			<delete-collection-form v-if="deleteCollection" />
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
@@ -16,6 +29,30 @@ export default {
 		NewCollectionForm,
 		EditCollectionForm,
 		DeleteCollectionForm
+	},
+	data() {
+		return {
+			newCollection: true,
+			editCollection: false,
+			deleteCollection: false
+		}
+	},
+	methods: {
+		showNewCollection: function() {
+			this.editCollection = false
+			this.deleteCollection = false
+			this.newCollection = true
+		},
+		showEditCollection: function() {
+			this.editCollection = true
+			this.deleteCollection = false
+			this.newCollection = false
+		},
+		showDeleteCollection: function() {
+			this.editCollection = false
+			this.deleteCollection = true
+			this.newCollection = false
+		}
 	}
 }
 </script>
